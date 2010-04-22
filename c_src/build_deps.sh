6b22198dfb2fce0d1c3f1c424e5f4d98c2525926
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2010-04-22 16:42:53 +0200

    Adapt c_src/build_deps.sh to support building libre2.so.0

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -17,12 +17,22 @@ case "$1" in
     ERL_ARCH=`erl -noinput +B -eval \
       'io:format("~B",[8 * erlang:system_info(wordsize)]),halt(0).'`
     CXXFLAGS="-Wall -O3 -fPIC"
+    MAKE_SHARED_LIBRARY="g++"
+
+    if [ `uname -s` = Darwin ]; then
+      MAKE_SHARED_LIBRARY="$MAKE_SHARED_LIBRARY -dynamiclib"
+    else
+      MAKE_SHARED_LIBRARY="$MAKE_SHARED_LIBRARY -shared -Wl,-soname,libre2.so.0"
+    fi
+
 
     if [ $ERL_ARCH -eq 32 ]; then
       CXXFLAGS="$CXXFLAGS -m32"
+      MAKE_SHARED_LIBRARY="$MAKE_SHARED_LIBRARY -m32"
     fi
 
-    (cd re2 && make -j2 CXXFLAGS="$CXXFLAGS")
+    (cd re2 && make -j2 CXXFLAGS="$CXXFLAGS" \
+      MAKE_SHARED_LIBRARY="$MAKE_SHARED_LIBRARY")
 
     ;;
 esac
