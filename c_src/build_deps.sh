235b90769ca5a2ffbf1f7963b87606f0600d1ebb
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2010-05-01 14:33:59 +0200

    Adapt to upstream RE2 build changes

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -16,22 +16,24 @@ case "$1" in
 
     ERL_ARCH=`erl -noinput +B -eval \
       'io:format("~B",[8 * erlang:system_info(wordsize)]),halt(0).'`
-    CXXFLAGS="-Wall -O3 -fPIC"
+    CXXFLAGS="-Wall -O3 -fPIC -pthread"
+    LDFLAGS="-pthread"
     MAKE_SHARED_LIBRARY="g++"
 
     if [ `uname -s` = Darwin ]; then
-      MAKE_SHARED_LIBRARY="$MAKE_SHARED_LIBRARY -dynamiclib"
+      MAKE_SHARED_LIBRARY="$MAKE_SHARED_LIBRARY -dynamiclib $LDFLAGS \
+        -exported_symbols_list libre2.symbols.darwin"
     else
-      MAKE_SHARED_LIBRARY="$MAKE_SHARED_LIBRARY -shared -Wl,-soname,libre2.so.0"
+      MAKE_SHARED_LIBRARY="$MAKE_SHARED_LIBRARY -shared \
+        -Wl,-soname,libre2.so.0,--version-script=libre2.symbols $LDFLAGS"
     fi
-
 
     if [ $ERL_ARCH -eq 32 ]; then
       CXXFLAGS="$CXXFLAGS -m32"
       MAKE_SHARED_LIBRARY="$MAKE_SHARED_LIBRARY -m32"
     fi
 
-    (cd re2 && make -j2 CXXFLAGS="$CXXFLAGS" \
+    (cd re2 && make -j2 CXXFLAGS="$CXXFLAGS" LDFLAGS="$LDFLAGS" \
       MAKE_SHARED_LIBRARY="$MAKE_SHARED_LIBRARY")
 
     ;;
