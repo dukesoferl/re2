7a584dd37819f3ec8b330076749d500ddaf993e7
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2014-02-10 17:12:15 +0100

    Update copyright years

--- c_src/re2_nif.cpp
+++ c_src/re2_nif.cpp
@@ -1,4 +1,4 @@
-// Copyright 2010-2011 Tuncer Ayaz. All Rights Reserved.
+// Copyright 2010-2014 Tuncer Ayaz. All Rights Reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
