f5130cf1c80c76da081e51ae413e7baf164a7ac4
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2014-02-10 17:52:31 +0100

    Clean up and fix specs

--- src/re2.erl
+++ src/re2.erl
@@ -15,9 +15,11 @@
          replace/3,
          replace/4]).
 
--export_type([compile_option/0,
+-export_type([
+              compile_option/0,
               match_option/0,
-              replace_option/0]).
+              replace_option/0
+             ]).
 
 -on_load(load_nif/0).
 
@@ -36,54 +38,65 @@ load_nif() ->
               end,
     erlang:load_nif(filename:join(PrivDir, "re2_nif"), 0).
 
+%% NOTE: compiled_regex/0 is not declared as -opaque because:
+%% 1. If you declare :: any() as an opaque type, then the compiler will
+%%    complain that it's underspecified like this:
+%%    Warning: opaque type compiled_regex() is underspecified and
+%%    therefore meaningless
+%% 2. Opaque types only make sense when exported, and the compiler will
+%%    rightly complain about that as follows:
+%%    Warning: opaque type compiled_regex() is not exported
 
--type uncompiled_pattern() :: iodata().
--opaque compiled_pattern() :: binary().
--type input()           :: iodata().
--type pattern()         :: uncompiled_pattern() | compiled_pattern().
--type replacement()     :: iodata().
+-type uncompiled_regex() :: iodata().
+-type compiled_regex() :: any().
+%% compiled_regex/0 is an opaque datatype containing a compiled regex
+%% created by enif_make_resource(). Resources are totally opaque,
+%% which means the actual type is undefined. In the current Erlang VM
+%% they behave like empty (&lt;&lt;&gt;&gt;) binaries on the Erlang
+%% side, but that could change in future releases. Therefore, make no
+%% assumptions, and always treat it as an opaque datatype.
+-type subject() :: iodata().
+-type regex() :: uncompiled_regex() | compiled_regex().
+-type replacement() :: iodata().
 
--type match_option()    :: 'caseless' | {'offset', non_neg_integer()}
-                         | {'capture', value_spec()}
-                         | {'capture', value_spec(), value_spec_type()}.
--type value_spec()      :: 'all' | 'all_but_first' | 'first' | 'none'
-                         | [value_id()].
+-type match_option() :: 'caseless' | {'offset', non_neg_integer()}
+                      | {'capture', value_spec()}
+                      | {'capture', value_spec(), value_spec_type()}.
+-type value_spec() :: 'all' | 'all_but_first' | 'first' | 'none'
+                    | [value_id()].
 -type value_spec_type() :: 'index' | 'binary'.
--type value_id()        :: non_neg_integer() | string() | atom().
--type match_result()    :: 'match' | 'nomatch' | {'match', list()}
-                         | {'error', any()}.
+-type value_id() :: non_neg_integer() | string() | atom().
+-type match_result() :: 'match' | 'nomatch' | {'match', list()}
+                      | {'error', atom()}.
 
--type compile_option()  :: 'caseless' | {'max_mem', non_neg_integer()}.
--type compile_result()  :: {'ok', compiled_pattern()} | {'error', any()}.
+-type compile_error() :: {'error', {atom(), string(), string()}}.
+-type compile_option() :: 'caseless' | {'max_mem', non_neg_integer()}.
+-type compile_result() :: {'ok', compiled_regex()} | compile_error().
 
--type replace_option()  :: 'global'.
--type replace_result()  :: iodata() | {'error', any()} | 'error'.
+-type replace_option() :: 'global'.
+-type replace_result() :: binary() | {'error', atom()} | 'error'.
 
--spec compile(Pattern::uncompiled_pattern()) -> compile_result().
+-spec compile(uncompiled_regex()) -> compile_result().
 compile(_) ->
     ?nif_stub.
 
--spec compile(Pattern::uncompiled_pattern(),
-              Options::[compile_option()]) -> compile_result().
+-spec compile(uncompiled_regex(), [compile_option()]) -> compile_result().
 compile(_,_) ->
     ?nif_stub.
 
--spec match(Input::input(), Pattern::pattern()) -> match_result().
+-spec match(subject(), regex()) -> match_result().
 match(_,_) ->
     ?nif_stub.
 
--spec match(Input::input(), Pattern::pattern(),
-            Options::[match_option()]) -> match_result().
+-spec match(subject(), regex(), [match_option()]) -> match_result().
 match(_,_,_) ->
     ?nif_stub.
 
--spec replace(Input::input(), Pattern::pattern(),
-              Replacement::replacement()) -> replace_result().
+-spec replace(subject(), regex(), replacement()) -> replace_result().
 replace(_,_,_) ->
     ?nif_stub.
 
--spec replace(Input::input(), Pattern::pattern(),
-              Replacement::replacement(),
-              Options::[replace_option()]) -> replace_result().
+-spec replace(subject(), regex(), replacement(),
+              [replace_option()]) -> replace_result().
 replace(_,_,_,_) ->
     ?nif_stub.
