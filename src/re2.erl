cb084df35b61a5a49dfc156542f4ce779ca1a2b1
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2016-02-11 21:53:24 +0100

    Update copyright years

--- src/re2.erl
+++ src/re2.erl
@@ -1,4 +1,4 @@
-%% Copyright 2010-2015 Tuncer Ayaz. All Rights Reserved.
+%% Copyright 2010-2016 Tuncer Ayaz. All Rights Reserved.
 %% Use of this source code is governed by a BSD-style
 %% license that can be found in the LICENSE file.
 %%
