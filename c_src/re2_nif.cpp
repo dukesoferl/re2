f29f5d3750710399ae85c769f70bbd7c884c2e7d
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2016-09-18 12:19:02 +0200

    Use more correct delimiters for erl_nif.h include
    We don't #include a local copy of erl_nif.h, so all that "erl_nif.h"
    does is: (1) fail to find it outside of -I dirs, (2) then treat it as if
    it was written like <erl_nif.h>. Using <erl_nif.h> skips (1).

--- c_src/re2_nif.cpp
+++ c_src/re2_nif.cpp
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-#include "erl_nif.h"
+#include <erl_nif.h>
 
 #include <re2/re2.h>
 #include <map>
