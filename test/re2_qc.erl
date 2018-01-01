11c01e7a6a4931799a4afe540280d4b58b118413
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2018-01-01 05:27:31 +0000

    Update copyright years

--- test/re2_qc.erl
+++ test/re2_qc.erl
@@ -1,4 +1,4 @@
-%% Copyright (c) 2013-2017 Tuncer Ayaz. All Rights Reserved.
+%% Copyright (c) 2013-2018 Tuncer Ayaz. All Rights Reserved.
 %% Use of this source code is governed by a BSD-style
 %% license that can be found in the LICENSE file.
 
