5eee506835dec4c2bf34ac2268f9dd9592d4db87
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2019-02-10 13:48:21 +0000

    Fix Windows build

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-
 set -e
 
 test `basename $PWD` != "c_src" && cd c_src
+
+case "$(uname -o)" in
+    Msys*|Cygwin*)
+        IS_WINDOWS=yes
+        echo "===> IS_WINDOWS: $IS_WINDOWS"
+        ;;
+    *)
+        IS_WINDOWS=no;;
+esac
 
 case "$1" in
     clean)
@@ -10,11 +18,17 @@ case "$1" in
         ;;
 
     *)
-        if [ x"$RE2_DEBUG" = x"1" ]; then
-            LIBRE2="obj/dbg/libre2.a"
+        if [ x"$IS_WINDOWS" = x"yes" ]; then
+            RE2_ARCHIVE=re2.lib
         else
-            LIBRE2="obj/libre2.a"
+            RE2_ARCHIVE=libre2.a
         fi
+        if [ x"$RE2_DEBUG" = x"1" ]; then
+            LIBRE2="obj/dbg/$RE2_ARCHIVE"
+        else
+            LIBRE2="obj/$RE2_ARCHIVE"
+        fi
+        unset RE2_ARCHIVE
         test -f re2/$LIBRE2 && exit 0
 
         RE2_REV=${RE2_REV:-2019-01-01}
@@ -30,12 +44,33 @@ case "$1" in
         test -d re2 || git clone $RE2_URL re2
         (cd re2 && git fetch --all && git checkout $RE2_REV)
 
-        CXXFLAGS="-Wall -O3 -fPIC -pthread -std=c++11 -m$ERLANG_ARCH"
-        CXX="${CXX:-c++} -m$ERLANG_ARCH"
-        which gmake 1>/dev/null 2>/dev/null && MAKE=gmake
-        MAKE=${MAKE:-make}
-        MAKEFLAGS=${MAKEFLAGS:--j2}
-        export MAKEFLAGS
-        ($MAKE -C re2 $LIBRE2 CXX="$CXX" CXXFLAGS="$CXXFLAGS")
+        if [ x"$IS_WINDOWS" = x"yes" ]; then
+            (
+                cd re2
+                mkdir -p windows_build
+                cd windows_build
+
+                GENERATOR="Visual Studio 15 2017"
+                if [ x"$RE2_DEBUG" = x"1" ]; then
+                    BUILD_TYPE=Debug
+                else
+                    BUILD_TYPE=Release
+                fi
+                cmake -D CMAKE_BUILD_TYPE=$BUILD_TYPE -G "$GENERATOR" -A x64 ..
+                cmake --build . --config $BUILD_TYPE
+                cd ..
+                LIB=windows_build/$BUILD_TYPE/re2.lib
+                mkdir -p $(dirname $LIBRE2)
+                cp $LIB $LIBRE2
+            )
+        else
+            CXXFLAGS="-Wall -O3 -fPIC -pthread -std=c++11 -m$ERLANG_ARCH"
+            CXX="${CXX:-c++} -m$ERLANG_ARCH"
+            type gmake 1>/dev/null 2>/dev/null && MAKE=gmake
+            MAKE=${MAKE:-make}
+            MAKEFLAGS=${MAKEFLAGS:--j2}
+            export MAKEFLAGS
+            ($MAKE -C re2 $LIBRE2 CXX="$CXX" CXXFLAGS="$CXXFLAGS")
+        fi
         ;;
 esac
