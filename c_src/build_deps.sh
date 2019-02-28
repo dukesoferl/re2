0b21eb54ac4a30735ee3b9fed02eb6b37d39c012
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2019-02-28 22:19:46 +0000

    c_src/build_deps.sh: fix formatting

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -5,37 +5,37 @@ set -e
 test `basename $PWD` != "c_src" && cd c_src
 
 case "$1" in
-  clean)
-    rm -rf re2/obj
-    ;;
+    clean)
+        rm -rf re2/obj
+        ;;
 
-  *)
-    if [ x"$RE2_DEBUG" = x"1" ]; then
-        LIBRE2="obj/dbg/libre2.a"
-    else
-        LIBRE2="obj/libre2.a"
-    fi
-    test -f re2/$LIBRE2 && exit 0
+    *)
+        if [ x"$RE2_DEBUG" = x"1" ]; then
+            LIBRE2="obj/dbg/libre2.a"
+        else
+            LIBRE2="obj/libre2.a"
+        fi
+        test -f re2/$LIBRE2 && exit 0
 
-    RE2_REV=${RE2_REV:-2019-01-01}
-    case $(git config --get remote.origin.url) in
-        git@github.com*|https://github.com*|git://github.com*)
-            RE2_DEFAULT_URL=https://github.com/google/re2
-            ;;
-        *)
-            RE2_DEFAULT_URL=https://code.googlesource.com/re2
-            ;;
-    esac
-    RE2_URL=${RE2_URL:-$RE2_DEFAULT_URL}
-    test -d re2 || git clone $RE2_URL re2
-    (cd re2 && git fetch --all && git checkout $RE2_REV)
+        RE2_REV=${RE2_REV:-2019-01-01}
+        case $(git config --get remote.origin.url) in
+            git@github.com*|https://github.com*|git://github.com*)
+                RE2_DEFAULT_URL=https://github.com/google/re2
+                ;;
+            *)
+                RE2_DEFAULT_URL=https://code.googlesource.com/re2
+                ;;
+        esac
+        RE2_URL=${RE2_URL:-$RE2_DEFAULT_URL}
+        test -d re2 || git clone $RE2_URL re2
+        (cd re2 && git fetch --all && git checkout $RE2_REV)
 
-    CXXFLAGS="-Wall -O3 -fPIC -pthread -std=c++11 -m$ERLANG_ARCH"
-    CXX="${CXX:-c++} -m$ERLANG_ARCH"
-    which gmake 1>/dev/null 2>/dev/null && MAKE=gmake
-    MAKE=${MAKE:-make}
-    MAKEFLAGS=${MAKEFLAGS:--j2}
-    export MAKEFLAGS
-    ($MAKE -C re2 $LIBRE2 CXX="$CXX" CXXFLAGS="$CXXFLAGS")
-    ;;
+        CXXFLAGS="-Wall -O3 -fPIC -pthread -std=c++11 -m$ERLANG_ARCH"
+        CXX="${CXX:-c++} -m$ERLANG_ARCH"
+        which gmake 1>/dev/null 2>/dev/null && MAKE=gmake
+        MAKE=${MAKE:-make}
+        MAKEFLAGS=${MAKEFLAGS:--j2}
+        export MAKEFLAGS
+        ($MAKE -C re2 $LIBRE2 CXX="$CXX" CXXFLAGS="$CXXFLAGS")
+        ;;
 esac
